ef276145c016852363e977c47ca76b1c23f7849c
Shurgannn <46814630+Shurgannn@users.noreply.github.com>
2019-02-27 13:15:18 +0300

    Add files via upload

--- requests_2.py
+++ requests_2.py
@@ -3,7 +3,8 @@ import requests
 API_KEY = 'trnsl.1.1.20161025T233221Z.47834a66fd7895d0.a95fd4bfde5c1794fa433453956bd261eae80152'
 URL = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
 
-def translate_it(file_to_open, new_file, to_lang):
+
+def translate_it(file_to_open, new_file, from_lang, to_lang='ru'):
     """
     https://translate.yandex.net/api/v1.5/tr.json/translate ?
     key=<API-ключ>
@@ -16,18 +17,15 @@ def translate_it(file_to_open, new_file, to_lang):
     :return:
     """
 
-    #params = {
+    # params = {
     #    'key': API_KEY,
     #    'text': text,
     #    'lang': 'ru-{}'.format(to_lang),
     #}
 
     with open(file_to_open) as file_path:
-        text = []
-        for t in file_path:
-            text.append(t)
-        params = {'key': API_KEY, 'text': text, 'lang': '{}-ru'.format(to_lang)}
-
+        text = file_path.read()
+        params = {'key': API_KEY, 'text': text, 'lang': '{}-{}'.format(from_lang, to_lang)}
         response = requests.get(URL, params=params)
         json_ = response.json()
 
@@ -39,6 +37,6 @@ def translate_it(file_to_open, new_file, to_lang):
     return ''.join(json_['text'])
 
 
-#print(translate_it('В настоящее время доступна единственная опция — признак включения в ответ автоматически определенного языка переводимого текста. Этому соответствует значение 1 этого параметра.', 'en'))
+# print(translate_it('В настоящее время доступна единственная опция — признак включения в ответ автоматически определенного языка переводимого текста. Этому соответствует значение 1 этого параметра.', 'en'))
 translate_it('DE', 'DE_RU', 'de')
 
